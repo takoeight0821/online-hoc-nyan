70a527873d67a1e3cf1b5f2dfbb22f1e8ed006b9
Yuya Kono <takohati0821@gmail.com>
2019-09-08 01:06:52 +0900

    remove unused code

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeOperators         #-}
 module Main where
 
@@ -13,7 +14,6 @@ import           GHC.Generics
 import           Network.HTTP.Media       ((//), (/:))
 import           Network.Wai.Handler.Warp
 import           Servant
-import           Servant.JS
 import           System.Process
 import           Web.FormUrlEncoded       (FromForm (..), parseUnique)
 
@@ -26,11 +26,9 @@ data Output = Output { object :: String, compileError :: String }
 instance FromForm Compiler where
   fromForm form = Compiler <$> parseUnique "source" form
 
-type CompilerAPI = "compiler" :> ReqBody '[JSON, FormUrlEncoded] Compiler :> Post '[JSON] Output
-
 type API = Get '[HTML] BS.ByteString
-  :<|> "static" :> Raw
-  :<|> CompilerAPI
+           :<|> "static" :> Raw
+           :<|> "compiler" :> ReqBody '[JSON, FormUrlEncoded] Compiler :> Post '[JSON] Output
 
 data HTML
 
@@ -41,25 +39,17 @@ instance MimeRender HTML BS.ByteString where
   mimeRender _ bs = bs
 
 server :: BS.ByteString -> Server API
-server indexFile = index
+server indexFile = pure indexFile
                    :<|> serveDirectoryFileServer "./static"
                    :<|> compile
   where
-    index = return indexFile
     compile :: Compiler -> Handler Output
     compile (Compiler src) = do
       liftIO $ writeFile "./tmp.c" src
       (_, asm, err)<- liftIO $ readProcessWithExitCode "./hoc_nyan/hoc" ["./tmp.c"] ""
       return $ Output asm err
 
-compilerAPI :: Proxy CompilerAPI
-compilerAPI = Proxy
-
-api :: Proxy API
-api = Proxy
-
 main :: IO ()
 main = do
-  writeJSForAPI compilerAPI jquery "./static/client.js"
   indexFile <- BS.readFile "./static/index.html"
-  run 8080 (serve api (server indexFile))
+  run 8080 (serve (Proxy @API) (server indexFile))
